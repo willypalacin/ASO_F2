5f5e8c5cb4ce04ce631343cb7d5383d54b630059
RockyJames <jaume@campeny.net>
2020-01-07 13:43:11 +0100

    CHORE: Only log once implemented If the user is logged, he can't go to the log form before logout

--- cgi-bin/login-view.sh
+++ cgi-bin/login-view.sh
@@ -3,16 +3,68 @@
 echo "Content-type: text/html"
 echo ""
 
+echo '<html>'
+echo '    <head>'
+echo '      <title> Login </title>'
+echo '    </head>'
+echo '    <body>'
+
+
+hash=$(echo "$HTTP_COOKIE" | cut -c 13-)
+decrypt=$(echo "$hash" |openssl base64 -d|openssl enc -d -aes-256-cbc -k "ILOVU")
+username=$(echo "$decrypt" | awk {'print $1'} | sed 's/\r$//')
+password=$(echo "$decrypt" | awk {'print $2'} | sed 's/\r$//')
+
+if [ ! -z "$hash" ]; then
+    for user in $(cut -f1 -d: /etc/passwd)
+    do
+        if [ "$user" = "$username" ]; then
+            while IFS= read -r line
+            do
+                shadow_user=$(echo "$line" | awk -F : {'print $1'} | sed 's/\r$//')
+
+                if [ "$shadow_user" != "$username" ]; then
+                    continue
+                fi
+
+                shadow_pass=$(echo "$line" | awk -F : {'print $2'} | sed 's/\r$//')
+                sha_number=$(echo $shadow_pass | awk -F $ {'print $2'} | sed 's/\r$//')
+                sha_salt=$(echo $shadow_pass | awk -F $ {'print $3'} | sed 's/\r$//')
+
+                case $sha_number in
+                    1)
+                        password_encrypted=$(echo $password | openssl passwd -1 -salt $sha_salt -stdin)
+                        ;;
+                    5)
+                        password_encrypted=$(echo $password | openssl passwd -5 -salt $sha_salt -stdin)
+                        ;;
+                    6)
+                        password_encrypted=$(echo $password | openssl passwd -6 -salt $sha_salt -stdin)
+                        ;;
+                esac
+                break
+            done < /etc/shadow
+        fi
+    done
+    if [ "$shadow_pass" = "$password_encrypted" ]; then
+        echo '      <form name="Logged" id="myForm" target="_myFrame" action="/cgi-bin/menu.sh" method="GET" ENCTYPE="text/plain">'
+        echo '          <div align=center><input type="hidden" value="Tornar Menu"></div>'
+        echo '      </form>'
+        echo '      <script type="text/javascript">'
+        echo '          document.Logged.submit();'
+        echo '      </script>'
+        echo '  </body>'
+        echo '</html>'
+        exit 0
+    fi
+fi
+
 read response
 error=$(echo $response | awk -F "&" {'print $1'} | awk -F = {'print $2'} | sed 's/\r$//')
 username=$(echo $response | awk -F "&" {'print $2'} | awk -F = {'print $2'} | sed 's/\r$//')
 password=$(echo $response | awk -F "&" {'print $3'} | awk -F = {'print $2'} | sed 's/\r$//')
 
-echo '<html>'
-echo '  <head>'
-echo '      <title> Login </title>'
-echo '  </head>'
-echo '  <body>'
+
 echo '      <br>'
 echo '      <h4 align=center>SIGN IN</h4>'
 echo '      <form action="/cgi-bin/login-logic.sh" method="POST" ENCTYPE="text/plain">'
